12dbf76bab1231a6c738a1c2892489904fa717ba
harshad1462 <harshad.ph2004@gmail.com>
2025-09-07 01:06:14 +0530

    home component page done

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils'
 
 export default function ChatContainer({ selectedUser, setSelectedUser }) {
+    const scrollEnd =useRef();
+    useEffect(()=>{
+        if(scrollEnd.current){
+            scrollEnd.current.scrollIntoView({behavior : "smooth"})
+        }
+    },[])
     return selectedUser ? (
         <div className="h-full overflow-scroll relative backdrop-blur-lg">
             {/* header part */}
@@ -32,12 +39,25 @@ export default function ChatContainer({ selectedUser, setSelectedUser }) {
                             <div className="text-center text-xs">
                                 <img src={msg.senderId ==="680f50e4f10f3cd28382ecf9" ? assets.avatar_icon :assets.profile_martin} alt=""
                                 className='w-7 rounded-full' />
+                                <p className="text-grey-500 text-white">{formatMessageTime(msg.createdAt)}</p>
                             </div>
-
                             </div>
                     ))}
+                    <div ref={scrollEnd}></div>
                  </div>
+                 {/* Bottom Area */}
+                 <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+                    <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+                        <input type="text" placeholder='Send a Message' 
+                        className='my-1 flex-1 text-sm border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+                        <input type="file" name="" id="image" accept='image/png, image/jpeg' hidden/>
+                        <label htmlFor="image">
+                            <img src={assets.gallery_icon} className='w-5 mr-2 cursor-pointer' alt="" />
+                        </label>
+                    </div>
+                    <img src={assets.send_button} className='w-7 cursor-pointer' alt="" />
 
+                 </div>
         </div>
     ) : (
         <div className="flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
